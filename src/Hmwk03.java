ae1c7badda933c88ee846de90ad984bf3b7a97a9
Riley Flint <riley.scott.flint@gmail.com>
2017-09-18 18:30:29 -0500

    Initial Commit Remove addString Method and get length from commandline arguments

--- src/Hmwk03.java
+++ src/Hmwk03.java
@@ -1,21 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Hmwk03 {
-    public static void main() {
-        String[] test = {"one", "two", "three"};
-        String[] added = addItem(test, "four");
-        for (String item : added) {
-            System.out.println(item);
+    public static void main(String[] args) {
+        File inputFile = new File(fetchFileName(args));
+        String[] passwords = readFromFile(inputFile);
+
+        for (String password : passwords) {
+            System.out.println(password);
         }
     }
-    private static String[] addItem(String[] array, String add) {
-        int newLength = array.length+1;
-        String[] result = new String[newLength];
 
-        for (int i = 0; i < array.length; i++) {
-            result[i] = array[i];
+    private static String[] readFromFile(File inputFile) {
+        String[] result = new String[0];
+
+        try {
+            Scanner input = new Scanner(inputFile);
+            if (input.hasNext()) {
+                while (input.hasNext()) {
+                    result = addString(result, input.next());
+                }
+            } else {
+                System.out.printf("File \"%s\" is empty.\n", inputFile);
+                System.exit(1);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.printf("File \"%s\" not found.\n", inputFile);
+            System.exit(1);
         }
 
-        result[newLength] = add;
+        return result;
+    }
 
+    private static String fetchFileName(String[] args) {
+        String result = "common.txt";
+        if (args.length > 0) {
+            result = args[0];
+        }
+        return result;
+    }
+
+    private static String[] addString(String[] array, String add) {
+        String[] result = new String[array.length+1];
+        System.arraycopy(array, 0, result, 0, array.length);
+        result[array.length] = add;
         return result;
     }
 }
