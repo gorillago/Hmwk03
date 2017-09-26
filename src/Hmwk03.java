bc7da98246febeb8b30448127b73ef38df62b1c8
Riley Flint <riley.scott.flint@gmail.com>
2017-09-25 19:55:30 -0500

    Completed Homework 3

--- src/Hmwk03.java
+++ src/Hmwk03.java
@@ -1,3 +1,11 @@
+//Riley Flint
+
+//This program takes in two commandline arguments. The first argument is the file name (defaults to common.txt) and
+//the second argument is the maximum number of passwords that you wish to read in from the file (defaults to 100).
+//The program will take a user input after reading the file and will ask them to enter a password. After the user
+//enters the password, it will check if it is contained in the array of strings read from the file. The program will
+//endlessly continue to ask the user to enter passwords until the user enters the word "stop."
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -5,21 +13,23 @@ import java.util.Scanner;
 public class Hmwk03 {
     public static void main(String[] args) {
         File inputFile = new File(fetchFileName(args));
-        String[] passwords = readFromFile(inputFile);
+        int max = fetchMaximumSize(args);
+        String[] passwords = new String[max];
+        int count = readFromFile(passwords, inputFile);
 
-        for (String password : passwords) {
-            System.out.println(password);
-        }
+        checkPasswords(passwords, count);
     }
 
-    private static String[] readFromFile(File inputFile) {
-        String[] result = new String[0];
+    private static int readFromFile(String[] passwords, File inputFile) {
+        int count = 0;
+        int max = passwords.length;
 
         try {
             Scanner input = new Scanner(inputFile);
             if (input.hasNext()) {
-                while (input.hasNext()) {
-                    result = addString(result, input.next());
+                while ((count < max) && input.hasNext()) {
+                    passwords[count] = input.next().trim();
+                    count++;
                 }
             } else {
                 System.out.printf("File \"%s\" is empty.\n", inputFile);
@@ -30,7 +40,8 @@ public class Hmwk03 {
             System.exit(1);
         }
 
-        return result;
+
+        return count;
     }
 
     private static String fetchFileName(String[] args) {
@@ -41,10 +52,55 @@ public class Hmwk03 {
         return result;
     }
 
-    private static String[] addString(String[] array, String add) {
-        String[] result = new String[array.length+1];
-        System.arraycopy(array, 0, result, 0, array.length);
-        result[array.length] = add;
+    private static int fetchMaximumSize(String[] args) {
+        int result = 100;
+        if (args.length > 1) {
+            try {
+                result = Integer.parseInt(args[1]);
+            } catch(NumberFormatException e) {
+                System.out.printf("\"%s\" is not a valid integer.\n", args[1]);
+                System.exit(1);
+            }
+        }
         return result;
+    }
+
+    private static String getPassword() {
+        System.out.print("Please enter your password: ");
+        Scanner input = new Scanner(System.in);
+        String password = input.next().trim();
+        return password;
+    }
+
+    private static boolean containsPassword(String password, String[] passwords, int count) {
+        boolean found = false;
+        int current = 0;
+        while(!found && (current < count)) {
+            found = passwords[current].equalsIgnoreCase(password);
+            current++;
+        }
+        return found;
+    }
+
+    private static void checkPasswords(String[] passwords, int count) {
+        boolean stopped = false;
+
+        System.out.println();
+
+        System.out.println("Type \"Stop\" to end search.");
+
+        while (!stopped) {
+            String password = getPassword();
+            stopped = password.equalsIgnoreCase("stop");
+            if (stopped) {
+                System.out.println("Ending search.");
+            } else {
+                if (containsPassword(password, passwords, count)) {
+                    System.out.println("\""+password+"\" is in the list of common passwords.");
+                } else {
+                    System.out.println("\""+password+"\" is not in the list of common passwords.");
+                }
+            }
+        }
     }
 }
